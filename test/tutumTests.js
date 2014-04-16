da55e8d8bb14ed6542d51c20e9beaebdb53bb2f7
Golo Roden <webmaster@goloroden.de>
2014-04-16 16:13:06 +0200

    Implemented authenticate function.

--- test/tutumTests.js
+++ test/tutumTests.js
@@ -5,5 +5,40 @@ var assert = require('node-assertthat');
 var tutum = require('../lib/tutum');
 
 suite('tutum', function () {
+  test('is an object.', function () {
+    assert.that(tutum, is.ofType('object'));
+  });
 
+  suite('authenticate', function () {
+    test('is a function.', function () {
+      assert.that(tutum.authenticate, is.ofType('function'));
+    });
+
+    test('throws an error if options are missing.', function () {
+      assert.that(function () {
+        tutum.authenticate();
+      }, is.throwing('Authentication options are missing.'));
+    });
+
+    test('throws an error if options do not contain the username.', function () {
+      assert.that(function () {
+        tutum.authenticate({
+          apiKey: 'bar'
+        });
+      }, is.throwing('Username is missing.'));
+    });
+
+    test('throws an error if options do not contain the API key.', function () {
+      assert.that(function () {
+        tutum.authenticate({
+          username: 'foo'
+        });
+      }, is.throwing('API key is missing.'));
+    });
+
+    test('returns a session object.', function () {
+      var session = tutum.authenticate({ username: 'foo', apiKey: 'bar' });
+      assert.that(session, is.ofType('object'));
+    });
+  });
 });
